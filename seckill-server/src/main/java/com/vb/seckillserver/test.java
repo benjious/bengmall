cee7c75e93afc2bd5737bad9d1498355fd3d9208
chenjiongzhen <893326146@qq.com>
2019-05-29 20:01:29 +0800

    opt

---
+++ seckill-server/src/main/java/com/vb/seckillserver/test.java
@@ -0,0 +1,11 @@
+package com.vb.seckillserver;
+
+import sun.net.www.http.HttpClient;
+
+public class test {
+    private static HttpClient client;
+
+    public static void main(String[] args){
+
+    }
+}
